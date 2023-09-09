e7aa314dee21e723b8ef9d3052ce290cda07dfb6
lav200 <122050925+lav200@users.noreply.github.com>
2023-09-09 17:19:21 +0530

    Updated Files

--- Laptop.py
+++ Laptop.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 # import the model
-pipe = pickle.load(open('pipe1.pkl','rb'))
+pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
 page_bg_img  = """
